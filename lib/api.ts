ed2ed805a28382067fce7518b7bf7a863540c9fa
Tanya <yakovetsta1@gmail.com>
2026-04-04 20:28:06 -0300

    renew types/note.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { Note } from "@/types/notes";
+import type { Note } from "@/types/note";
 
 const BASE_URL = "https://notehub-public.goit.study/api";
 const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
